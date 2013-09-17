1d5829b5f506df66c294d63cb9669c9e4fae0685
thomas-voss <thomas.voss@canonical.com>
2013-09-17 07:17:38 +0200

    Ramp up acceptance testing.

--- tests/acceptance-tests/service.cpp
+++ tests/acceptance-tests/service.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright © 2013 Canonical Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by: Thomas Voß <thomas.voss@canonical.com>
+ */
+
 #include <com/ubuntu/music/service.h>
 #include <com/ubuntu/music/session.h>
 
@@ -16,7 +34,7 @@ TEST(MusicService, starting_playback_on_non_empty_playqueue_works)
     auto service = com::ubuntu::music::Service::stub();
     auto session = service->start_session();
 
-    
+    session->
 
     EXPECT_TRUE(session);
 }
