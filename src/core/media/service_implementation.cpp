79223de7a416121e3f117f76ee3556382794ae74
thomas-voss <thomas.voss@canonical.com>
2015-02-24 14:41:29 +0100

    We really should release the on request, not the off one.

--- src/core/media/service_implementation.cpp
+++ src/core/media/service_implementation.cpp
@@ -160,7 +160,7 @@ struct media::ServiceImplementation::Private
         }
         else if (state == media::RecordingState::stopped)
         {
-            display_state_lock->request_release(media::power::DisplayState::off);
+            display_state_lock->request_release(media::power::DisplayState::on);
         }
     }
 
