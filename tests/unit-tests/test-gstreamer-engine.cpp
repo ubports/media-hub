32f0ddace218756f6a4fd4ad5ac9525092de1688
Willem-Jan de Hoog <wdehoog@exalondelft.nl>
2020-11-05 07:16:13 -0500

    fake test to see if the http server is actually used

--- tests/unit-tests/test-gstreamer-engine.cpp
+++ tests/unit-tests/test-gstreamer-engine.cpp
@@ -462,6 +462,8 @@ TEST(GStreamerEngine, meta_data_extractor_supports_embedded_album_art)
             EXPECT_TRUE(headers.at("Cookie").count("A=B") == 1);
             EXPECT_TRUE(headers.at("Cookie").count("X=Y") == 1);
 
+	    EXPECT_TRUE(false) << "arrived before mg_send_file: " << test_file;
+
             mg_send_file(conn, test_file.c_str(), 0);
             return MG_MORE;
         }
