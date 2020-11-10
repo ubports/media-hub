1e7d29160ae20b1b3f1eb2fc1eb085c0938cb9dd
Willem-Jan de Hoog <wdehoog@exalondelft.nl>
2020-11-10 15:37:06 -0500

    use remote url, see if it helps

--- tests/unit-tests/test-gstreamer-engine.cpp
+++ tests/unit-tests/test-gstreamer-engine.cpp
@@ -440,7 +440,8 @@ TEST(GStreamerEngine, meta_data_extractor_supports_embedded_album_art)
     const std::string test_file{"/tmp/test-audio.ogg"};
     std::remove(test_file.c_str());
     ASSERT_TRUE(test::copy_test_media_file_to("test-audio.ogg", test_file));
-    const std::string test_audio_uri{"http://localhost:5000"};
+    //const std::string test_audio_uri{"http://localhost:5000"};
+    const std::string test_audio_uri{"http://www.wndbz.nl/ubuntu-touch/test-audio.ogg"};
     //const std::string test_audio_uri{"file:///tmp/test-audio.ogg"};
 
     // test server
