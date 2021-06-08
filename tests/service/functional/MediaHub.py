973430df53802a848829a99a492fdb91ed3dfa64
Alberto Mardegan <mardy@users.sourceforge.net>
2021-06-09 00:40:03 +0300

    tests: add a test for tracklist playback

--- tests/service/functional/MediaHub.py
+++ tests/service/functional/MediaHub.py
@@ -56,8 +56,10 @@ class Player(object):
     Stopped = 4
 
     def __init__(self, bus_obj, object_path):
-        service_name = ServiceName
-        session = bus_obj.get_object(service_name, object_path,
+        self.bus_obj = bus_obj
+        self.object_path = object_path
+        self.service_name = ServiceName
+        session = bus_obj.get_object(self.service_name, object_path,
                                      introspect=False)
         self.interface_name = 'org.mpris.MediaPlayer2.Player'
         self.__player = dbus.Interface(session, self.interface_name)
@@ -66,16 +68,49 @@ class Player(object):
             'org.freedesktop.DBus.Properties')
         self.props = {}
         self.__prop_callbacks = []
+        self.signals = []
+        self.__signal_callbacks = []
 
         self.loop = GLib.MainLoop()
         self.__properties.connect_to_signal('PropertiesChanged',
                                             self.__on_properties_changed)
+
+        self.__player.connect_to_signal(
+                'PlaybackStatusChanged',
+                lambda x: self.__on_signal('PlaybackStatusChanged', x))
+        self.__player.connect_to_signal(
+                'AboutToFinish',
+                lambda: self.__on_signal('AboutToFinish'))
+        self.__player.connect_to_signal(
+                'EndOfStream',
+                lambda: self.__on_signal('EndOfStream'))
 
     def __on_properties_changed(self, interface, changed, invalidated):
         assert interface == self.interface_name
         self.props.update(changed)
         for cb in self.__prop_callbacks:
             cb(interface, changed, invalidated)
+
+    def __on_signal(self, signal_name, *args):
+        print('Signal emitted: ' + signal_name)
+        self.signals.append([signal_name, *args])
+        for cb in self.__signal_callbacks:
+            cb(signal_name, *args)
+
+    def wait_for_signal(self, name, *args, timeout=3000):
+        match = [name, *args]
+        if match in self.signals:
+            return True
+
+        timer_id = GLib.timeout_add(timeout, self.loop.quit)
+        def check_value(signal_name, *signal_args):
+            if [name, *signal_args] == match:
+                GLib.source_remove(timer_id)
+                self.loop.quit()
+        self.on_signal(check_value)
+        self.loop.run()
+        self.unsubscribe_signal(check_value)
+        return True if match in self.signals else False
 
     def wait_for_prop(self, name, value, timeout=3000):
         if self.props.get(name) == value:
@@ -107,6 +142,26 @@ class Player(object):
 
     def unsubscribe_properties_changed(self, callback):
         self.__prop_callbacks.remove(callback)
+
+    def on_signal(self, callback):
+        self.__signal_callbacks.append(callback)
+
+    def unsubscribe_signal(self, callback):
+        self.__signal_callbacks.remove(callback)
+
+
+class TrackList(object):
+    End = '/org/mpris/MediaPlayer2/TrackList/NoTrack'
+
+    def __init__(self, player):
+        object_path = player.object_path + '/TrackList'
+        session = player.bus_obj.get_object(player.service_name, object_path,
+                                            introspect=False)
+        self.interface_name = 'org.mpris.MediaPlayer2.TrackList'
+        self.__track_list = dbus.Interface(session, self.interface_name)
+
+    def add_track(self, track_uri, position=End, set_as_current=False):
+        self.__track_list.AddTrack(track_uri, position, set_as_current)
 
 
 class MprisPlayer(Player):
